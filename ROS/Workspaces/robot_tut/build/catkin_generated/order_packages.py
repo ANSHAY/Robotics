e9ed8657e352637f67651926c9f0a692fa7b7a4b
Anshay <anshayagr@gmail.com>
2020-11-13 19:36:16 +0530

    Initial commit

---
+++ ROS/Workspaces/robot_tut/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/media/Git/GITHUB/Robotics/ROS/Workspaces/robot_tut/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/media/Git/GITHUB/Robotics/ROS/Workspaces/robot_tut/devel;/opt/ros/noetic'.split(';') if '/media/Git/GITHUB/Robotics/ROS/Workspaces/robot_tut/devel;/opt/ros/noetic' != '' else []
